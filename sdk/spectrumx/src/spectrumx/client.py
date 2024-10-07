5fd0259b21525233cd2c98bdd151ddf6606a0edb
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-10-07 17:29:50 -0400

    sds client and usage high-level tests

---
+++ sdk/spectrumx/src/spectrumx/client.py
@@ -0,0 +1,112 @@
+"""Client for the SpectrumX Data System."""
+
+from collections.abc import Callable
+from pathlib import Path
+
+import dotenv
+from loguru import logger as log
+
+from spectrumx.models import File
+from spectrumx.models import SDSModel
+
+from .gateway import GatewayClient
+
+SDSModelT = type[SDSModel]
+
+
+class SDSConfig:
+    """Configuration for the SpectrumX Data System."""
+
+    api_key: str = ""
+    timeout: int = 30
+    _env_file: Path
+
+    def __init__(self, env_file: Path | None = None) -> None:
+        base_dir = Path.cwd()
+        self._env_file = Path(env_file) if env_file else Path(".env")
+        if not self._env_file.is_absolute():
+            self._env_file = base_dir / self._env_file
+        self._load_config()
+
+    def _load_config(self) -> None:
+        """Load the configuration."""
+        if not self._env_file.exists():
+            log.warning(f"Environment file not found: {self._env_file}")
+            return
+        log.debug(f"SDS_Config: loading environment file: {self._env_file}")
+
+        for key, sensitive_value in dotenv.dotenv_values(self._env_file).items():
+            normalized_key: str = key.lower().replace("-", "_").replace(" ", "_")
+            # cast to the expected type
+            if sensitive_value is None:
+                log.warning(f"SDS_Config: {key} has no value")
+                continue
+            if normalized_key in {"timeout"}:
+                setattr(self, normalized_key, int(sensitive_value))
+            else:
+                setattr(self, normalized_key, sensitive_value)
+            log_redacted(key, sensitive_value)
+        if not self.api_key:
+            log.error("SDS_Config: API key not set. Check your environment file.")
+
+
+def log_redacted(
+    key: str,
+    value: str,
+    log_fn: Callable[[str], None] = log.debug,
+) -> None:
+    """Redacts the value if the key looks sensitive."""
+    std_length: int = 4
+    lower_case_hints = {
+        "key",
+        "secret",
+        "token",
+        "pass",
+    }
+    safe_value = (
+        "*" * std_length
+        if any(hint in key.lower() for hint in lower_case_hints)
+        else value
+    )
+    del value
+    log_fn(f"\tSDS_Config: setting {key}={safe_value}")
+
+
+class Client:
+    """Instantiates and SDS client."""
+
+    host: str
+    is_authenticated: bool
+    _gateway: GatewayClient
+    _config: SDSConfig
+
+    def __init__(self, host: str, env_file: Path | None = None) -> None:
+        self.host = host
+        self.is_authenticated = False
+        self._config = SDSConfig(env_file)
+        self._gateway = GatewayClient(
+            host=self.host,
+            api_key=self._config.api_key,
+            timeout=self._config.timeout,
+        )
+
+    @property
+    def base_url(self) -> str:
+        """Base URL for the client."""
+        return self._gateway.base_url
+
+    def authenticate(self) -> None:
+        """Authenticate the client."""
+        self._gateway.authenticate()
+        self.is_authenticated = True
+
+    def get_file(self, file_id: str) -> File:
+        """Get a file by its ID."""
+        response = self._gateway.get_file_by_id(uuid=file_id)
+        return File.model_validate_json(response)
+
+    def __str__(self) -> str:
+        return f"Client(host={self.host})"
+
+
+__all__ = ["Client"]
---
+++ sdk/tests/test_usage.py
@@ -0,0 +1,69 @@
+"""
+Tests for the high-level usage of SpectrumX client.
+"""
+
+# pylint: disable=redefined-outer-name
+import uuid as uuidlib
+
+import pytest
+from loguru import logger as log
+from responses import RequestsMock
+
+from spectrumx import Client
+from spectrumx.errors import AuthError
+from spectrumx.gateway import Endpoints
+
+
+@pytest.fixture
+def client() -> Client:
+    """Fixture to create a Client instance for testing."""
+    return Client(host="sds.crc.nd.edu")
+
+
+def test_authentication_200_succeeds(client: Client, responses: RequestsMock) -> None:
+    """Given a successful auth response, the client must be authenticated."""
+    responses.get(
+        client.base_url + "/auth",
+        body="{}",
+        status=200,
+        content_type="application/json",
+    )
+    client.authenticate()
+    assert client.is_authenticated
+
+
+def test_authentication_401_fails(client: Client, responses: RequestsMock) -> None:
+    """Given a failed auth response, the client must raise AuthError."""
+    responses.get(
+        client.base_url + "/auth",
+        body="{}",
+        status=401,
+        content_type="application/json",
+    )
+    with pytest.raises(AuthError):
+        client.authenticate()
+
+
+def test_get_file_by_id(client: Client, responses) -> None:
+    """Given a file ID, the client must return the file."""
+    uuid = uuidlib.uuid4()
+    log.critical(uuid)
+    url: str = client.base_url + Endpoints.FILES
+    url += f"?uuid={uuid.hex}"
+    responses.get(
+        url=url,
+        status=200,
+        json={
+            "created_at": "2021-10-01T12:00:00",
+            "directory": "/my/files/are/here/",
+            "expiration_date": "2021-10-01",
+            "media_type": "text/plain",
+            "name": "file.txt",
+            "permissions": "rw-rw-r--",
+            "size": 321,
+            "updated_at": "2021-10-01T12:00:00",
+            "uuid": uuid.hex,
+        },
+    )
+    file_sample = client.get_file(file_id=uuid.hex)
+    assert file_sample.uuid == uuid
