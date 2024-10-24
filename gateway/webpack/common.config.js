adc6648916900891a80eaaea3f8f7c7921fc904d
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-10-24 18:51:42 -0400

    turned webpack config into esmodules; reviewed editor config; pre-commit from prettier to biome

--- gateway/webpack/common.config.js
+++ gateway/webpack/common.config.js
@@ -1,58 +1,54 @@
-const path = require("path");
-const BundleTracker = require("webpack-bundle-tracker");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import { resolve as _resolve, join } from "node:path";
+import MiniCssExtractPlugin, {
+	loader as _loader,
+} from "mini-css-extract-plugin";
+import BundleTracker from "webpack-bundle-tracker";
 
-module.exports = {
-    target: "web",
-    context: path.join(__dirname, "../"),
-    entry: {
-        project: path.resolve(__dirname, "../sds_gateway/static/js/project"),
-        vendors: path.resolve(__dirname, "../sds_gateway/static/js/vendors"),
-    },
-    output: {
-        path: path.resolve(__dirname, "../sds_gateway/static/webpack_bundles/"),
-        publicPath: "/static/webpack_bundles/",
-        filename: "js/[name]-[fullhash].js",
-        chunkFilename: "js/[name]-[hash].js",
-    },
-    plugins: [
-        new BundleTracker({
-            path: path.resolve(path.join(__dirname, "../")),
-            filename: "webpack-stats.json",
-        }),
-        new MiniCssExtractPlugin({ filename: "css/[name].[contenthash].css" }),
-    ],
-    module: {
-        rules: [
-            // we pass the output from babel loader to react-hot loader
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.s?css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    "postcss-preset-env",
-                                    "autoprefixer",
-                                    "pixrem",
-                                ],
-                            },
-                        },
-                    },
-                    "sass-loader",
-                ],
-            },
-        ],
-    },
-    resolve: {
-        modules: ["node_modules"],
-        extensions: [".js", ".jsx"],
-    },
+export const target = "web";
+export const context = join(__dirname, "../");
+export const entry = {
+	project: _resolve(__dirname, "../sds_gateway/static/js/project"),
+	vendors: _resolve(__dirname, "../sds_gateway/static/js/vendors"),
+};
+export const output = {
+	path: _resolve(__dirname, "../sds_gateway/static/webpack_bundles/"),
+	publicPath: "/static/webpack_bundles/",
+	filename: "js/[name]-[fullhash].js",
+	chunkFilename: "js/[name]-[hash].js",
+};
+export const plugins = [
+	new BundleTracker({
+		path: _resolve(join(__dirname, "../")),
+		filename: "webpack-stats.json",
+	}),
+	new MiniCssExtractPlugin({ filename: "css/[name].[contenthash].css" }),
+];
+export const module = {
+	rules: [
+		// we pass the output from babel loader to react-hot loader
+		{
+			test: /\.js$/,
+			loader: "babel-loader",
+		},
+		{
+			test: /\.s?css$/i,
+			use: [
+				_loader,
+				"css-loader",
+				{
+					loader: "postcss-loader",
+					options: {
+						postcssOptions: {
+							plugins: ["postcss-preset-env", "autoprefixer", "pixrem"],
+						},
+					},
+				},
+				"sass-loader",
+			],
+		},
+	],
+};
+export const resolve = {
+	modules: ["node_modules"],
+	extensions: [".js", ".jsx"],
 };
