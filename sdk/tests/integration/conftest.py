d9affab88c94e4d6681fb7bc6bfa9a21271395cc
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-11-08 10:59:03 -0500

    test for large file upload

--- sdk/tests/integration/conftest.py
+++ sdk/tests/integration/conftest.py
@@ -1,6 +1,7 @@
 """Fixtures and utilities for integration tests."""
 
 # better traceback formatting with rich, if available
+import os
 from collections.abc import Generator
 from pathlib import Path
 
@@ -13,13 +14,13 @@ from spectrumx.client import Client
 @pytest.fixture
 def _integration_setup_teardown() -> Generator[None, None, None]:
     # setup code for integration tests
-    log.error("Setting up for integration test")
+    log.debug("Setting up for integration test")
 
     # yield control to the test
     yield
 
     # teardown code for integration tests
-    log.error("Tearing down after integration test")
+    log.debug("Tearing down after integration test")
 
 
 @pytest.fixture
@@ -79,3 +80,27 @@ def integration_client() -> Client:
     )
     assert _integration_client.dry_run is False, "Dry run mode should be disabled."
     return _integration_client
+
+
+def random_bytes_generator(
+    size: int, chunk: int = 1024
+) -> Generator[bytes, None, None]:
+    """Generates random binary data for tests."""
+    for _ in range(size // chunk):
+        yield os.urandom(chunk)
+    yield os.urandom(size % chunk)
+
+
+@pytest.fixture
+@pytest.mark.slow
+def temp_large_binary_file(tmp_path: Path, target_size_mb: int = 2048) -> Path:
+    """Fixture to create a temporary large binary file."""
+    large_binary_file = tmp_path / "large_binary_file"
+    byte_generator = random_bytes_generator(1024 * 1024 * target_size_mb)
+    with large_binary_file.open("wb") as f_ptr:
+        for chunk in byte_generator:
+            f_ptr.write(chunk)
+    log.warning(
+        f"Created large binary file of size: {large_binary_file.stat().st_size:,} bytes"
+    )
+    return large_binary_file
--- sdk/tests/integration/test_file_ops.py
+++ sdk/tests/integration/test_file_ops.py
@@ -71,3 +71,35 @@ def test_bulk_file_upload(integration_client: Client, temp_file_tree: Path) -> N
         if upload_result:
             continue
         pytest.fail(f"File upload failed: {upload_result}")
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures("_integration_setup_teardown")
+@pytest.mark.usefixtures("_without_responses")
+@pytest.mark.parametrize(
+    "_without_responses",
+    [
+        [
+            f"https://sds.crc.nd.edu:443/api/{API_TARGET_VERSION}/assets/files",
+            f"http://localhost:80/api/{API_TARGET_VERSION}/assets/files",
+        ]
+    ],
+    indirect=True,
+)
+def test_large_file_upload(
+    integration_client: Client, temp_large_binary_file: Path
+) -> None:
+    """Tests uploading a large file to SDS."""
+    sds_path = Path("/")
+    local_file = construct_file(temp_large_binary_file, sds_path=sds_path)
+    uploaded_file = integration_client.upload_file(
+        temp_large_binary_file, sds_path=sds_path
+    )
+    assert uploaded_file.is_sample is False, "Sample file returned."
+    for attr in uploaded_file.__dict__:
+        log.debug(f"\t{attr:>15} = {uploaded_file.__dict__[attr]}")
+    assert uploaded_file is not None, "File upload failed."
+    assert isinstance(uploaded_file.uuid, uuid.UUID), "UUID not set."
+    assert uploaded_file.size == local_file.size, "Size mismatch."
+    assert len(uploaded_file.sum_blake3 or "") == BLAKE3_HEX_LEN, "Checksum not set."
+    assert uploaded_file.sum_blake3 == local_file.sum_blake3, "Checksum mismatch."
