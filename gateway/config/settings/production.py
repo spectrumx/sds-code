4c662f8033e2fd168f4cceca7a3c3c4c99fa2ca9
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-11-08 19:04:27 -0500

    re-enabled sec settings

--- gateway/config/settings/production.py
+++ gateway/config/settings/production.py
@@ -45,37 +45,37 @@ CACHES = {
 
 # ⚠️ Setting overrides for PRODUCTION
 
-# # SECURITY
-# # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-# SECURE_PROXY_SSL_HEADER: tuple[str, ...] = ("HTTP_X_FORWARDED_PROTO", "https")
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-# SECURE_SSL_REDIRECT: bool = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
-# # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE: bool = True
-# # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
-# SESSION_COOKIE_NAME: str = "__Secure-sessionid"
-# # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE: bool = True
-# # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
-# CSRF_COOKIE_NAME: str = "__Secure-csrftoken"
-# # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
-# # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-# # !!! Setting this incorrectly can irreversibly (for some time) break your site !!!
-# SECURE_HSTS_SECONDS: int = 60
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
-# SECURE_HSTS_INCLUDE_SUBDOMAINS: bool = env.bool(
-#     "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
-#     default=True,
-# )
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
-# SECURE_HSTS_PRELOAD: bool = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
-# # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
-# SECURE_CONTENT_TYPE_NOSNIFF: bool = env.bool(
-#     "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF",
-#     default=True,
-# )
+# SECURITY
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER: tuple[str, ...] = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT: bool = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE: bool = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
+SESSION_COOKIE_NAME: str = "__Secure-sessionid"
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE: bool = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
+CSRF_COOKIE_NAME: str = "__Secure-csrftoken"
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+# !!! Setting this incorrectly can irreversibly (for some time) break your site !!!
+SECURE_HSTS_SECONDS: int = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS: bool = env.bool(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
+    default=True,
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD: bool = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF: bool = env.bool(
+    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF",
+    default=True,
+)
 
 # ⚠️ Setting overrides for PRODUCTION
 
