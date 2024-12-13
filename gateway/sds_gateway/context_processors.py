e54eb24e7fd60bfb6a5b421d4240323e51029662
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-12-13 15:52:19 -0500

    gwy: first system notification about January maintenance

--- gateway/sds_gateway/context_processors.py
+++ gateway/sds_gateway/context_processors.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
+from typing import Literal
 
 from django.http import HttpRequest
 
@@ -13,6 +14,8 @@ class Notification:
     user_message: str
     admin_message: str
     expires_on: datetime
+    level: Literal["info", "warning", "danger"] = "warning"
+    # level is one of the alert-* classes from Bootstrap
 
 
 eastern = timezone(timedelta(hours=-5))
@@ -23,8 +26,10 @@ def system_notifications(_request: HttpRequest) -> dict[str, list[Notification]]
     notifications = [
         Notification(
             user_message="SDS is scheduled for maintenance on "
-            "January 11, 2025 from 7AM to 11AM ET.",
+            "January 11, 2025 from 7AM to 11AM ET and it might be "
+            "unavailable during this time period.",
             admin_message="Scheduled CRC maintenance.",
+            level="warning",
             expires_on=datetime(2025, 1, 11, 11, 0, 0, tzinfo=eastern),
         ),
     ]
