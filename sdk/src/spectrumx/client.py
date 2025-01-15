27263d41406eaf37ffc918f46fdad5d45b071da3
Lucas Parzianello <lucaspar@users.noreply.github.com>
2025-01-15 18:24:46 -0500

    sdk: refactored file implementations into a dedicated module

--- sdk/src/spectrumx/client.py
+++ sdk/src/spectrumx/client.py
@@ -2,15 +2,10 @@
 
 import logging
 import os
-import tempfile
-import time
 import uuid
 from collections.abc import Callable
 from collections.abc import Mapping
 from dataclasses import dataclass
-from enum import Enum
-from enum import auto
-from multiprocessing.synchronize import RLock
 from pathlib import Path
 from typing import TYPE_CHECKING
 from typing import Any
@@ -84,16 +79,6 @@ class DeprecatedOption:
         return warning
 
 
-class FileUploadMode(Enum):
-    """Modes for uploading files to SDS."""
-
-    SKIP = auto()  # no upload or update needed
-    UPDATE_METADATA_ONLY = auto()  # no file contents, update an existing file entry
-    UPLOAD_CONTENTS_AND_METADATA = auto()  # create a new file uploading everything
-    UPLOAD_METADATA_ONLY = auto()  # no file contents, create a new file entry
-    # file contents are immutable, so there is no "UPDATE_CONTENTS_ONLY"
-
-
 class SDSConfig:
     """Configuration for the SpectrumX Data System."""
 
@@ -117,8 +102,26 @@ class SDSConfig:
         Args:
             env_file:   Path to the environment file to load the config from.
             env_config: Overrides for the environment file.
-            host:       The host to connect to (the config file has priority over this).
+            sds_host:   The host to connect to (the config file has priority over this).
             verbose:    Show which config files are loaded and which attributes are set.
+        """
+        self.sds_host = sds_host
+        self.init_config(env_file=env_file, env_config=env_config, verbose=verbose)
+
+    def init_config(
+        self,
+        *,
+        env_file: Path | None,
+        env_config: Mapping[str, Any] | None,
+        verbose: bool,
+    ) -> None:
+        """Initializes the client config, with `env_config` taking precedence.
+
+        Args:
+            env_file:   Path to the environment file to load the config from.
+            env_config: Overrides for the environment file.
+            verbose:    Show which config files are loaded and which attributes are set.
+
         """
         base_dir = Path.cwd()
         self._env_file = Path(env_file) if env_file else Path(".env")
@@ -229,6 +232,11 @@ class Client:
         env_config: Mapping[str, Any] | None = None,
         verbose: bool = False,
     ) -> None:
+        # avoids circular import
+        from spectrumx.api import sds_files as _sds_files
+
+        self._sds_files = _sds_files
+
         self.host = host
         self.is_authenticated = False
         self.verbose = verbose
@@ -246,6 +254,13 @@ class Client:
             timeout=self._config.timeout,
             verbose=self.verbose,
         )
+        self._issue_user_alerts()
+
+    def __str__(self) -> str:
+        return f"Client(host={self.host})"
+
+    def _issue_user_alerts(self) -> None:
+        """Logs important messages on initialization."""
         if __version__.startswith("0.1."):
             log_user_warning(
                 "This version of the SDK is in early development. "
@@ -284,7 +299,8 @@ class Client:
     def authenticate(self) -> None:
         """Authenticate the client."""
         if self.dry_run:
-            log_user("Dry run enabled: authenticated")
+            log_user("Dry run is enabled: assuming successful authentication")
+            log_user("To authenticate against SDS, set Client.dry_run to False")
         else:
             log.warning("Dry run DISABLED: authenticating")
             self._gateway.authenticate()
@@ -303,16 +319,7 @@ class Client:
             The file instance, or a sample file if in dry run mode.
         """
 
-        uuid_to_set: UUID4 = (
-            uuid.UUID(file_uuid) if isinstance(file_uuid, str) else file_uuid
-        )
-
-        if not self.dry_run:
-            file_bytes = self._gateway.get_file_by_id(uuid=uuid_to_set.hex)
-            return File.model_validate_json(file_bytes)
-
-        log_user("Dry run enabled: a sample file is being returned instead")
-        return files.generate_sample_file(uuid_to_set)
+        return self._sds_files.get_file(client=self, file_uuid=file_uuid)
 
     def download(
         self,
@@ -322,7 +329,7 @@ class Client:
         skip_contents: bool = False,
         overwrite: bool = False,
         verbose: bool = True,
-    ) -> list[Result]:
+    ) -> list[Result[File]]:
         """Downloads files from SDS.
 
         Args:
@@ -413,16 +420,7 @@ class Client:
         Returns:
             A paginator for the files in the given SDS path.
         """
-        if self.dry_run:
-            log_user("Dry run enabled: files will be simulated")
-        pagination: Paginator[File] = Paginator(
-            gateway=self._gateway,
-            sds_path=sds_path,
-            Entry=File,
-            dry_run=self.dry_run,
-        )
-
-        return pagination
+        return self._sds_files.list_files(client=self, sds_path=sds_path)
 
     def download_file(
         self,
@@ -449,92 +447,14 @@ class Client:
         Returns:
             The file instance with updated attributes, or a sample when in dry run.
         """
-        if isinstance(file_instance, File):
-            if file_instance.uuid is None:
-                msg = "The file passed is a local reference and cannot be downloaded."
-                raise ValueError(msg)
-            if file_instance.local_path is None and warn_missing_path:
-                msg = (
-                    "The file instance passed is missing a local path to "
-                    "download to. A temporary file will be created on disk."
-                )
-                log_user_warning(msg)
-            if skip_contents:
-                msg = (
-                    "A file instance was provided and skip_contents "
-                    "is True: nothing to download."
-                )
-                log_user_warning(msg)
-                return file_instance
-            valid_uuid = file_instance.uuid
-            valid_local_path_or_none = file_instance.local_path
-        else:
-            if file_uuid is None:
-                msg = "Expected a file instance or UUID to download."
-                raise ValueError(msg)
-            if to_local_path is None and warn_missing_path:
-                msg = "The file will be downloaded as temporary."
-                log_user_warning(msg)
-
-            valid_local_path_or_none = Path(to_local_path) if to_local_path else None
-
-            # download
-            valid_uuid: UUID4 = (
-                uuid.UUID(file_uuid) if isinstance(file_uuid, str) else file_uuid
-            )
-            if self.dry_run:
-                time.sleep(0.08)
-                return files.generate_sample_file(valid_uuid)
-
-            assert not self.dry_run, "Internal error: expected dry run to be disabled."
-            file_bytes = self._gateway.get_file_by_id(uuid=valid_uuid.hex)
-            file_instance = File.model_validate_json(file_bytes)
-
-        if not skip_contents:
-            downloaded_path = self._download_file_contents(
-                file_uuid=valid_uuid,
-                target_path=valid_local_path_or_none,
-                contents_lock=file_instance.contents_lock,  # pyright: ignore[reportPrivateUsage]
-            )
-            file_instance.local_path = downloaded_path
-        return file_instance
-
-    def _download_file_contents(
-        self,
-        *,
-        file_uuid: UUID4 | str,
-        contents_lock: RLock,
-        target_path: Path | None = None,
-    ) -> Path:
-        """Downloads the contents of a file from SDS to a location on disk.
-
-        If target_path is not provided, a temporary file is created.
-        When provided, the parent of target_path will be created if it does not exist.
-
-        Args:
-            file_uuid:      The UUID of the file to download from SDS.
-            target_path:    The local path to save the downloaded file to.
-        Returns:
-            The local path to the downloaded file.
-        """
-        if target_path is None:
-            file_desc, file_name = tempfile.mkstemp()
-            os.close(file_desc)
-            target_path = Path(file_name)
-        target_path = Path(target_path)
-        uuid_to_set: UUID4 = (
-            uuid.UUID(file_uuid) if isinstance(file_uuid, str) else file_uuid
+        return self._sds_files.download_file(
+            client=self,
+            file_instance=file_instance,
+            file_uuid=file_uuid,
+            to_local_path=to_local_path,
+            skip_contents=skip_contents,
+            warn_missing_path=warn_missing_path,
         )
-        if not self.dry_run:
-            target_path.parent.mkdir(parents=True, exist_ok=True)
-            with target_path.open(mode="wb") as file_ptr, contents_lock:
-                for chunk in self._gateway.get_file_contents_by_id(
-                    uuid=uuid_to_set.hex
-                ):
-                    file_ptr.write(chunk)
-        else:
-            log_user(f"Dry run enabled: file would be saved as {target_path}")
-        return target_path
 
     def upload(
         self,
@@ -558,7 +478,11 @@ class Client:
         for file_path in prog_bar:
             try:
                 result = Result(
-                    value=self.upload_file(local_file=file_path, sds_path=sds_path)
+                    value=self._sds_files.upload_file(
+                        client=self,
+                        local_file=file_path,
+                        sds_path=sds_path,
+                    )
                 )
             except SDSError as err:
                 log_user_error(f"Upload failed: {err}")
@@ -588,182 +512,12 @@ class Client:
         Returns:
             The file instance with updated attributes, or a sample when in dry run.
         """
-        # validate inputs
-        if not isinstance(local_file, (File, Path, str)):
-            msg = (
-                "file_path must be a Path, str, or "
-                f"File instance, not {type(local_file)}"
-            )
-            raise TypeError(msg)
-        local_file = Path(local_file) if isinstance(local_file, str) else local_file
-        sds_path = Path(sds_path)
 
-        # construct the file instance if needed
-        if isinstance(local_file, File):
-            file_instance = local_file.model_copy()
-            if file_instance.directory:
-                composed_sds_path = sds_path / file_instance.directory
-                file_instance.directory = composed_sds_path
-        else:
-            file_instance = files.construct_file(local_file, sds_path=sds_path)
-        del local_file
-
-        return self.__upload_file_mux(file_instance)
-
-    def __upload_file_mux(self, file_instance: File) -> File:
-        """Uploads a file instance to SDS, choosing the right upload mode."""
-        file_path = file_instance.local_path
-        # check whether sds already has this file for this user
-        upload_mode, asset_id = self.__get_upload_mode_and_asset(
-            file_instance=file_instance
+        return self._sds_files.upload_file(
+            client=self,
+            local_file=local_file,
+            sds_path=sds_path,
         )
-
-        if self.dry_run:
-            log_user(f"Dry run enabled: skipping upload of '{file_path}'")
-            return file_instance
-
-        match upload_mode:
-            case FileUploadMode.SKIP:
-                log_user(f"Skipping upload of existing '{file_path}'")
-                return file_instance
-            case FileUploadMode.UPLOAD_CONTENTS_AND_METADATA:
-                log_user(f"Uploading C&M '{file_path}'")
-                file_created_response = self._gateway.upload_new_file(
-                    file_instance=file_instance
-                )
-                uploaded_file = File.model_validate_json(file_created_response)
-                uploaded_file.local_path = file_instance.local_path
-                return uploaded_file
-            case FileUploadMode.UPLOAD_METADATA_ONLY:
-                log_user(f"Uploading metadata for '{file_path}'")
-                if asset_id is None:
-                    msg = "Expected an asset ID when uploading metadata only"
-                    raise SDSError(msg)
-                return self.__upload_new_file_metadata_only(
-                    file_instance=file_instance, sibling_uuid=asset_id
-                )
-            case FileUploadMode.UPDATE_METADATA_ONLY:  # pragma: no cover
-                log_user(f"Updating metadata for '{file_path}'")
-                assert (
-                    asset_id is not None
-                ), "Expected an asset ID when updating metadata"
-                return self.__update_existing_file_metadata_only(
-                    file_instance=file_instance, asset_id=asset_id
-                )
-            case _:  # pragma: no cover
-                msg = f"Unexpected upload mode: {upload_mode}"
-                raise SDSError(msg)
-
-    def __update_existing_file_metadata_only(
-        self, file_instance: File, asset_id: UUID4 | None
-    ) -> File:
-        """UPDATES an existing file instance with new metadata.
-
-        ---
-            Note: favor use of the safer `__upload_new_file_metadata_only()`,
-            which creates a new File entry reusing the contents of a sibling file.
-        ---
-
-        Useful when the files is already instantiated in SDS and the file
-            contents did not change: only the metadata needs to be updated.
-
-        Args:
-            file_instance:  The file instance with new metadata to update.
-        Returns:
-            The file instance with updated attributes.
-        """
-        if asset_id is not None:
-            file_instance.uuid = asset_id
-
-        if self.dry_run:
-            msg = (
-                "Dry run enabled: would update metadata "
-                f"only for file '{file_instance.uuid}'"
-            )
-            log_user(msg)
-            return file_instance
-
-        assert not self.dry_run, "Internal error: expected dry run to be disabled."
-        file_response = self._gateway.update_existing_file_metadata(
-            file_instance=file_instance
-        )
-        return File.model_validate_json(file_response)
-
-    def __upload_new_file_metadata_only(
-        self, file_instance: File, sibling_uuid: UUID4
-    ) -> File:
-        """UPLOADS a new file instance to SDS, skipping the file contents.
-
-        Useful when there are identical files in different locations: only their
-            metadata needs to be uploaded to SDS. There must be a sibling file
-            with the right contents, under the same user, already in SDS for this
-            method to succeed.
-
-        The sibling entry is not modified and doesn't need to exist locally.
-
-        Args:
-            file_instance:  The file instance with new metadata.
-            sibling_uuid:   The UUID of the file which contents are identical to.
-        Returns:
-            The file instance with updated attributes.
-        """
-        if self.dry_run:
-            log_user("Dry run enabled: uploading metadata only")
-            return file_instance
-
-        assert not self.dry_run, "Internal error: expected dry run to be disabled."
-        file_response = self._gateway.upload_new_file_metadata_only(
-            file_instance=file_instance, sibling_uuid=sibling_uuid
-        )
-        return File.model_validate_json(file_response)
-
-    def __get_upload_mode_and_asset(
-        self, file_instance: File
-    ) -> tuple[FileUploadMode, uuid.UUID | None]:
-        """Determines how to upload a file into SDS.
-
-        Args:
-            file_instance:  The file instance to get the upload mode for.
-        Returns:
-            The mode to upload the file in. Modes are:
-                FileUploadMode.SKIP
-                FileUploadMode.UPLOAD_CONTENTS_AND_METADATA
-                FileUploadMode.UPLOAD_METADATA_ONLY
-            Asset ID: The asset ID of the file in SDS, if it exists.
-
-        SKIP is used when the file already exists in SDS and no changes are needed.
-        UPLOAD_CONTENTS_AND_METADATA is used when the file is new or its contents
-            have changed; it will create a new file entry with new contents.
-        UPLOAD_METADATA_ONLY is used when the file contents are already in SDS, and
-            it will create a new file entry while reusing existing contents from a
-            sibling file.
-        ---
-        UPDATE_METADATA_ONLY is NOT returned here, as it might cause unintended changes.
-            For this reason, "update" methods should be explicitly called by UUID.
-        ---
-        A "File" in SDS has immutable contents, so there is no "UPDATE_CONTENTS_ONLY".
-        Note upload_* methods / modes create new assets in SDS, while update_* don't.
-        """
-        # always assume upload is needed in dry-run, since we can't contact the server
-        if self.dry_run:
-            return (
-                FileUploadMode.UPLOAD_CONTENTS_AND_METADATA,
-                None,
-            )
-
-        file_contents_check = self._gateway.check_file_contents_exist(
-            file_instance=file_instance
-        )
-        asset_id = file_contents_check.asset_id
-
-        if file_contents_check.file_exists_in_tree:
-            return FileUploadMode.SKIP, asset_id
-        if file_contents_check.file_contents_exist_for_user:
-            return FileUploadMode.UPLOAD_METADATA_ONLY, asset_id
-        return FileUploadMode.UPLOAD_CONTENTS_AND_METADATA, asset_id
-
-    def __str__(self) -> str:
-        return f"Client(host={self.host})"
 
 
 def _clean_config(
