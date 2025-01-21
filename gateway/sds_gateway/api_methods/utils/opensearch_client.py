3d25e04de1a87aa0a78fc980e8f312afc743e3d0
klpoland <kinsey.l.poland@gmail.com>
2025-01-21 09:21:51 -0500

    change http connection library, tls handshake error

--- gateway/sds_gateway/api_methods/utils/opensearch_client.py
+++ gateway/sds_gateway/api_methods/utils/opensearch_client.py
@@ -1,19 +1,16 @@
 from django.conf import settings
 from opensearchpy import OpenSearch
-from opensearchpy import RequestsHttpConnection
-from requests.auth import HTTPBasicAuth
+from opensearchpy.connection.http_urllib3 import Urllib3HttpConnection
 
 
 def get_opensearch_client():
     return OpenSearch(
         hosts=[{"host": settings.OPENSEARCH_HOST, "port": settings.OPENSEARCH_PORT}],
-        http_auth=HTTPBasicAuth(
-            settings.OPENSEARCH_USER,
-            settings.OPENSEARCH_PASSWORD,
-        ),
+        http_auth=(settings.OPENSEARCH_USER, settings.OPENSEARCH_PASSWORD),
         use_ssl=settings.OPENSEARCH_USE_SSL,
         verify_certs=settings.OPENSEARCH_VERIFY_CERTS,
+        ssl_assert_hostname=False,
         ssl_show_warn=False,
-        connection_class=RequestsHttpConnection,
+        connection_class=Urllib3HttpConnection,
         ca_certs=settings.OPENSEARCH_CA_CERTS,
     )
