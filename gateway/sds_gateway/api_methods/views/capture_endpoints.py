12100abe363eeef19b016791625c46fe8a324ab5
klpoland <kinsey.l.poland@gmail.com>
2025-01-27 22:43:00 -0500

    rh capture creation integration

--- gateway/sds_gateway/api_methods/views/capture_endpoints.py
+++ gateway/sds_gateway/api_methods/views/capture_endpoints.py
@@ -1,31 +1,26 @@
-import tempfile
 from pathlib import Path
 from typing import cast
 
 from django.shortcuts import get_object_or_404
 from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import (
-    OpenApiExample,
-    OpenApiParameter,
-    OpenApiResponse,
-    extend_schema,
-)
+from drf_spectacular.utils import OpenApiExample
+from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.utils import OpenApiResponse
+from drf_spectacular.utils import extend_schema
 from opensearchpy import exceptions as os_exceptions
-from rest_framework import status, viewsets
+from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 import sds_gateway.api_methods.utils.swagger_example_schema as example_schema
 from sds_gateway.api_methods.authentication import APIKeyAuthentication
-from sds_gateway.api_methods.helpers.extract_drf_metadata import (
-    validate_metadata_by_channel,
-)
-from sds_gateway.api_methods.helpers.index_handling import index_capture_metadata
-from sds_gateway.api_methods.helpers.reconstruct_file_tree import reconstruct_tree
+from sds_gateway.api_methods.helpers.index_handling import handle_metadata
 from sds_gateway.api_methods.models import Capture
+from sds_gateway.api_methods.models import CaptureType
+from sds_gateway.api_methods.serializers.capture_serializers import CaptureGetSerializer
 from sds_gateway.api_methods.serializers.capture_serializers import (
-    CaptureGetSerializer,
     CapturePostSerializer,
 )
 from sds_gateway.users.models import User
@@ -40,6 +35,7 @@ class CaptureViewSet(viewsets.ViewSet):
         responses={
             201: CaptureGetSerializer,
             400: OpenApiResponse(description="Bad Request"),
+            503: OpenApiResponse(description="OpenSearch service unavailable"),
         },
         examples=[
             OpenApiExample(
@@ -61,61 +57,59 @@ class CaptureViewSet(viewsets.ViewSet):
     def create(self, request: Request) -> Response:
         # channel whose data/metadata to capture
         channel = request.data.get("channel", None)
+        capture_type = request.data.get("capture_type", None)
         # path to directory that contains the channel dirs
         requested_top_level_dir = Path(request.data.get("top_level_dir", ""))
         requester = cast(User, request.user)
 
-        # Ensure the top_level_dir is not a relative path
-        if not requested_top_level_dir.is_absolute():
-            msg = "Relative paths are not allowed."
-            return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Resolve the top_level_dir to an absolute path
+        # Validate path and permissions
+        user_files_dir = Path(f"/files/{requester.email}").resolve(strict=False)
         resolved_top_level_dir = requested_top_level_dir.resolve(strict=False)
 
-        # Ensure the resolved path is within the user's files directory
-        user_files_dir = Path(f"/files/{requester.email}").resolve(strict=False)
-        if not resolved_top_level_dir.is_relative_to(user_files_dir):
-            msg = f"The top_level_dir must be in the user's files directory: /files/{requester.email}"  # noqa: E501
+        if (
+            not requested_top_level_dir.is_absolute()
+            or not resolved_top_level_dir.is_relative_to(user_files_dir)
+        ):
+            msg = (
+                "The top_level_dir must be an absolute path within: "
+                f"/files/{requester.email}"
+            )
             return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Validate and create capture
         request.data["owner"] = requester.id
-        post_serializer = CapturePostSerializer(
-            data=request.data,
-        )
-        if post_serializer.is_valid():
-            post_serializer.save()
-        else:
+        post_serializer = CapturePostSerializer(data=request.data)
+        if not post_serializer.is_valid():
             return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        # get capture object from serializer
+        post_serializer.save()
         capture_data = dict(post_serializer.data)
-        capture = Capture.objects.get(
-            uuid=capture_data["uuid"],
-            owner=requester,
-        )
+        capture = Capture.objects.get(uuid=capture_data["uuid"], owner=requester)
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            tmp_dir_path, files_to_connect = reconstruct_tree(
-                target_dir=Path(temp_dir),
-                top_level_dir=requested_top_level_dir,
-                owner=requester,
+        # Validate capture type and handle metadata
+        if capture_type not in CaptureType.__members__.values():
+            msg = f"Invalid capture type: {capture_type}"
+            return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            handle_metadata(
+                capture,
+                requested_top_level_dir,
+                CaptureType(capture_type),
+                channel,
+                requester,
             )
+        except ValueError as e:
+            msg = f"Error handling metadata for capture '{capture.uuid}': {e}"
+            return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
+        except os_exceptions.ConnectionError as e:
+            msg = f"Error connecting to OpenSearch: {e}"
+            return Response({"detail": msg}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-            for cur_file in files_to_connect:
-                cur_file.capture = capture
-                cur_file.save()
-
-            if not isinstance(channel, str):
-                msg = "Channel must be a string."
-                return Response({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
-
-            validated_metadata = validate_metadata_by_channel(tmp_dir_path, channel)
-            index_capture_metadata(capture, validated_metadata)
-
-        get_serializer = CaptureGetSerializer(capture)
-
-        return Response(get_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            CaptureGetSerializer(capture).data,
+            status=status.HTTP_201_CREATED,
+        )
 
     @extend_schema(
         parameters=[
