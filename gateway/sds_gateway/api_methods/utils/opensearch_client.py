cdeec69232e7494f121200c074e65a41698246ae
klpoland <kinsey.l.poland@gmail.com>
2025-01-30 10:22:33 -0500

    revert opensearch client changes 2

--- gateway/sds_gateway/api_methods/utils/opensearch_client.py
+++ gateway/sds_gateway/api_methods/utils/opensearch_client.py
@@ -1,16 +1,19 @@
 from django.conf import settings
 from opensearchpy import OpenSearch
-from opensearchpy.connection.http_urllib3 import Urllib3HttpConnection
+from opensearchpy import RequestsHttpConnection
+from requests.auth import HTTPBasicAuth
 
 
 def get_opensearch_client():
     return OpenSearch(
         hosts=[{"host": settings.OPENSEARCH_HOST, "port": settings.OPENSEARCH_PORT}],
-        http_auth=(settings.OPENSEARCH_USER, settings.OPENSEARCH_PASSWORD),
+        http_auth=HTTPBasicAuth(
+            settings.OPENSEARCH_USER,
+            settings.OPENSEARCH_PASSWORD,
+        ),
         use_ssl=settings.OPENSEARCH_USE_SSL,
         verify_certs=settings.OPENSEARCH_VERIFY_CERTS,
-        ssl_assert_hostname=False,
         ssl_show_warn=False,
-        connection_class=Urllib3HttpConnection,
+        connection_class=RequestsHttpConnection,
         ca_certs=settings.OPENSEARCH_CA_CERTS,
     )
