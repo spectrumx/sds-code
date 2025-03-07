c1a3bf17032e3961e7bd0a48d6bf0ba837c9ac06
Lucas Parzianello <lucaspar@users.noreply.github.com>
2025-03-07 16:35:27 -0500

    gwy: reducing logging from the verbose boto modules

--- gateway/sds_gateway/api_methods/apps.py
+++ gateway/sds_gateway/api_methods/apps.py
@@ -1,9 +1,21 @@
+import logging
+
 from django.apps import AppConfig
+from loguru import logger as log
 
 
 class ApiMethodsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sds_gateway.api_methods"
 
-    def ready(self):
+    def ready(self) -> None:
         import sds_gateway.api_methods.schema  # noqa: F401
+
+        silence_unwanted_logs()
+
+
+def silence_unwanted_logs() -> None:
+    """Usually for modules that are too verbose."""
+    log.info("Silencing unwanted logs.")
+    logging.getLogger("botocore").setLevel(logging.ERROR)
+    logging.getLogger("boto3").setLevel(logging.ERROR)
