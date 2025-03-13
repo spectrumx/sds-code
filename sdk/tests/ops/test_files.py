fbb07de3ab1a25bf5603cec2c45df3e42355fecd
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-13 09:28:16 -0400

    sdk: allow marking tests for platform specific tests

--- sdk/tests/ops/test_files.py
+++ sdk/tests/ops/test_files.py
@@ -7,6 +7,7 @@ import uuid as uuidlib
 from datetime import datetime
 from pathlib import Path
 
+import pytest
 from responses import RequestsMock
 from spectrumx import Client
 from spectrumx.gateway import API_TARGET_VERSION
@@ -35,6 +36,8 @@ def _get_content_check_endpoint(client: Client) -> str:
     )
 
 
+@pytest.mark.linux
+@pytest.mark.darwin
 def test_get_file_permissions(temp_file_empty: Path) -> None:
     """Test get_file_permissions for many permission combinations."""
     chmod_combos = {
