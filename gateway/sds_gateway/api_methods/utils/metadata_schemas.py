bfc74af33229655d189980b8f9a4179e147f8da8
klpoland <kinsey.l.poland@gmail.com>
2025-03-18 09:30:25 -0400

    change number typing

--- gateway/sds_gateway/api_methods/utils/metadata_schemas.py
+++ gateway/sds_gateway/api_methods/utils/metadata_schemas.py
@@ -221,7 +221,7 @@ rh_capture_index_mapping = {
         "type": "float",
     },
     "altitude": {
-        "type": "float",
+        "type": "double",
     },
     "mac_address": {
         "type": "keyword",
