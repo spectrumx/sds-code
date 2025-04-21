45b57fab40f5a4a6aa63c467b27c4c062768d6ff
skradha <skradha26@gmail.com>
2025-04-21 13:04:55 -0400

    sds-ui updates

--- gateway/sds_gateway/users/urls.py
+++ gateway/sds_gateway/users/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .api.views import GetAPIKeyView
 from .views import user_detail_view
 from .views import user_file_detail_view
-from .views import user_file_list_view
 from .views import user_generate_api_key_view
 from .views import user_redirect_view
 from .views import user_update_view
+from .views import ListCapturesView
 
 app_name = "users"
 urlpatterns = [
@@ -14,7 +14,7 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
     path("generate-api-key/", user_generate_api_key_view, name="generate_api_key"),
-    path("file-list/", user_file_list_view, name="file_list"),
+    path("file-list", ListCapturesView.as_view(), name="file_list"),
     path("file-detail/<uuid:uuid>/", user_file_detail_view, name="file_detail"),
     # Used by SVI Server to get API key for a user
     path("get-svi-api-key/", GetAPIKeyView.as_view(), name="get_svi_api_key"),
