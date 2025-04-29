388b53c620ae227ce3917753557673d9d311ce9f
Lucas Parzianello <lucaspar@users.noreply.github.com>
2025-04-29 17:45:35 -0400

    gwy: pyright false positive; reverted type change

--- gateway/sds_gateway/api_methods/admin.py
+++ gateway/sds_gateway/api_methods/admin.py
@@ -5,14 +5,14 @@ from sds_gateway.api_methods import models
 
 # Register your models here.
 @admin.register(models.File)
-class FileAdmin(admin.ModelAdmin[models.File]):
+class FileAdmin(admin.ModelAdmin):  # pyright: ignore[reportMissingTypeArgument]
     list_display = ("name", "media_type", "size", "owner", "is_deleted")
     search_fields = ("checksum", "name", "media_type", "owner")
     ordering = ("-updated_at",)
 
 
 @admin.register(models.Capture)
-class CaptureAdmin(admin.ModelAdmin[models.Capture]):
+class CaptureAdmin(admin.ModelAdmin):  # pyright: ignore[reportMissingTypeArgument]
     list_display = ("channel", "capture_type", "index_name")
     search_fields = ("uuid", "channel", "index_name")
     list_filter = ("channel", "capture_type", "index_name")
@@ -20,7 +20,7 @@ class CaptureAdmin(admin.ModelAdmin[models.Capture]):
 
 
 @admin.register(models.Dataset)
-class DatasetAdmin(admin.ModelAdmin[models.Dataset]):
+class DatasetAdmin(admin.ModelAdmin):  # pyright: ignore[reportMissingTypeArgument]
     list_display = ("name", "doi")
     search_fields = ("name", "doi")
     ordering = ("-updated_at",)
