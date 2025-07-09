139495ae70a21248484f65cab182f4cb67c629bd
Le Li Kruczek <le.li.kruczek@gmail.com>
2025-07-09 10:44:13 -0400

    updated user api view and management pages

--- gateway/sds_gateway/users/urls.py
+++ gateway/sds_gateway/users/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
 from .api.views import GetAPIKeyView
+from .views import AllAPIKeysView
 from .views import ListCapturesView
+from .views import delete_api_key_view
+from .views import generate_api_key_form_view
+from .views import new_api_key_view
+from .views import revoke_api_key_view
+from .views import user_api_key_view
 from .views import user_captures_api_view
 from .views import user_dataset_download_view
 from .views import user_dataset_list_view
 from .views import user_detail_view
 from .views import user_file_detail_view
-from .views import user_generate_api_key_view
 from .views import user_group_captures_view
 from .views import user_redirect_view
 from .views import user_temporary_zip_download_view
@@ -18,7 +23,8 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
-    path("generate-api-key/", user_generate_api_key_view, name="generate_api_key"),
+    path("view-api-key/", user_api_key_view, name="view_api_key"),
+    path("new-api-key/", new_api_key_view, name="new_api_key"),
     path("file-list/", ListCapturesView.as_view(), name="file_list"),
     path("file-list/api/", user_captures_api_view, name="captures_api"),
     path("file-detail/<uuid:uuid>/", user_file_detail_view, name="file_detail"),
@@ -36,4 +42,12 @@ urlpatterns = [
     ),
     # Used by SVI Server to get API key for a user
     path("get-svi-api-key/", GetAPIKeyView.as_view(), name="get_svi_api_key"),
+    path("delete-api-key/", delete_api_key_view, name="delete_api_key"),
+    path("revoke-api-key/", revoke_api_key_view, name="revoke_api_key"),
+    path("all-api-keys/", AllAPIKeysView.as_view(), name="all_api_keys"),
+    path(
+        "generate-api-key-form/",
+        generate_api_key_form_view,
+        name="generate_api_key_form",
+    ),
 ]
