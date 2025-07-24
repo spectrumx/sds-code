6700e473b15d014e7f616352275886a006d89260
Connor Howington <connor.howington@gmail.com>
2025-07-24 17:38:12 -0400

    Set up django-cog

---
+++ gateway/sds_gateway/api_methods/__init__.py
@@ -0,0 +1,6 @@
+# Import cogs
+from .cog_pipelines import cleanup_temp_files_cog  # noqa: F401
+from .cog_pipelines import download_capture_files_cog  # noqa: F401
+from .cog_pipelines import process_waterfall_data_cog  # noqa: F401
+from .cog_pipelines import store_processed_data_cog  # noqa: F401
+from .cog_pipelines import update_processing_status_cog  # noqa: F401
