f4be74f01d9710904b107d849427473f748e31e9
klpoland <kinsey.l.poland@gmail.com>
2025-08-14 13:46:27 -0400

    add more test cases

--- gateway/sds_gateway/users/views.py
+++ gateway/sds_gateway/users/views.py
@@ -2560,6 +2560,7 @@ class ShareGroupListView(Auth0LoginRequiredMixin, UserSearchMixin, View):
                 elif permission.item_type == "capture":
                     item = Capture.objects.get(uuid=permission.item_uuid)
                 else:
+                    logger.warning(f"Unknown item type: {permission.item_type}")
                     continue  # Skip unknown item types
 
                 shared_assets.append(
