571f95f03eb59d512293d6b3d8fa425eb6b1c89c
srucker1 <srucker94@gmail.com>
2025-09-02 14:28:36 -0400

    comment out copy scripts and added github

--- gateway/compose/local/jupyter/jupyterhub_config.py
+++ gateway/compose/local/jupyter/jupyterhub_config.py
@@ -30,10 +30,12 @@ c.DockerSpawner.notebook_dir = os.environ.get(
 c.DockerSpawner.volumes = {
     "jupyterhub_data": "/home/jovyan/work",
     "/var/run/docker.sock": "/var/run/docker.sock",
-    "/Users/srucker1/Desktop/sds-code/gateway/compose/local/jupyter/sample_scripts": (
-        "/srv/jupyter/sample_scripts"
-    ),
-    "/Users/srucker1/Desktop/sds-code/gateway/scripts": "/srv/jupyter/scripts",
+    # TODO: Uncomment when ready to test script loading
+    # Local script mounting for proof of concept
+    # "/Users/srucker1/Desktop/sds-code/gateway/compose/local/jupyter/sample_scripts": (
+    #     "/srv/jupyter/sample_scripts"
+    # ),
+    # "/Users/srucker1/Desktop/sds-code/gateway/scripts": "/srv/jupyter/scripts",
 }
 c.DockerSpawner.extra_host_config = {
     "security_opt": ["label:disable"],
@@ -68,14 +70,30 @@ c.Spawner.environment = {"JUPYTER_ENABLE_LAB": "yes"}
 
 # Mount host directories into user containers (now handled in main volumes config)
 
-# Automatic script loading - copy repository scripts to user's home directory
-c.DockerSpawner.post_start_cmd = (
-    "bash -c 'pip install spectrumx && "
-    "mkdir -p /home/jovyan/work/scripts /home/jovyan/work/sample_scripts && "
-    "if [ ! -f /home/jovyan/work/scripts/.initialized ]; then "
-    "cp -r /srv/jupyter/scripts/* /home/jovyan/work/scripts/ && "
-    "cp -r /srv/jupyter/sample_scripts/* /home/jovyan/work/sample_scripts/ && "
-    "touch /home/jovyan/work/scripts/.initialized && "
-    "chmod -R 755 /home/jovyan/work/scripts && "
-    "chmod -R 755 /home/jovyan/work/sample_scripts; fi'"
-)
+# TODO: Uncomment when ready to test script loading
+# PROOF OF CONCEPT: Load scripts from BOTH local files AND GitHub repository
+# This allows testing both approaches to see which works better
+# c.DockerSpawner.post_start_cmd = (
+#     "bash -c 'pip install spectrumx && "
+#     "mkdir -p /home/jovyan/work/scripts /home/jovyan/work/sample_scripts && "
+#     "if [ ! -f /home/jovyan/work/scripts/.initialized ]; then "
+#     "echo \"=== PROOF OF CONCEPT: Loading scripts from multiple sources ===\" && "
+#     "echo \"1. Copying local scripts...\" && "
+#     "cp -r /srv/jupyter/scripts/* /home/jovyan/work/scripts/ 2>/dev/null || echo \"No local scripts found\" && "
+#     "cp -r /srv/jupyter/sample_scripts/* /home/jovyan/work/sample_scripts/ 2>/dev/null || echo \"No local sample_scripts found\" && "
+#     "echo \"2. Downloading scripts from GitHub...\" && "
+#     "cd /home/jovyan/work && "
+#     "git clone https://github.com/your-org/sds-gateway-scripts.git temp_scripts || echo \"GitHub repo not found, skipping...\" && "
+#     "if [ -d temp_scripts ]; then "
+#     "cp -r temp_scripts/scripts/* /home/jovyan/work/scripts/ 2>/dev/null || echo \"No GitHub scripts directory found\" && "
+#     "cp -r temp_scripts/sample_scripts/* /home/jovyan/work/sample_scripts/ 2>/dev/null || echo \"No GitHub sample_scripts directory found\" && "
+#     "rm -rf temp_scripts; fi && "
+#     "echo \"3. Setting permissions...\" && "
+#     "touch /home/jovyan/work/scripts/.initialized && "
+#     "chmod -R 755 /home/jovyan/work/scripts && "
+#     "chmod -R 755 /home/jovyan/work/sample_scripts && "
+#     "echo \"=== Script loading complete ===\"; fi'"
+# )
+
+# Minimal post-start command - just install spectrumx for now
+c.DockerSpawner.post_start_cmd = "pip install spectrumx"
