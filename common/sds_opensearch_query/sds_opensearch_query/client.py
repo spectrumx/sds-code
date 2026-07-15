423b3ad02185d1ac2057e6aaa52cc02c96ca651a
klpoland <kinsey.l.poland@gmail.com>
2026-07-15 16:55:06 -0400

    centralize opensearch client init

---
+++ common/sds_opensearch_query/sds_opensearch_query/client.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from loguru import logger as log
+from opensearchpy import OpenSearch
+from opensearchpy import RequestsHttpConnection
+from requests.auth import HTTPBasicAuth
+
+
+def build_opensearch_client(
+    *,
+    host: str,
+    port: int,
+    user: str = "",
+    password: str = "",
+    use_ssl: bool = False,
+    verify_certs: bool = False,
+    ca_certs: str | None = None,
+) -> OpenSearch:
+    payload = {
+        "hosts": [{"host": host, "port": port}],
+        "http_auth": HTTPBasicAuth(
+            user,
+            password,
+        ),
+        "use_ssl": use_ssl,
+        "verify_certs": verify_certs,
+        "ssl_show_warn": False,
+        "connection_class": RequestsHttpConnection,
+    }
+    if verify_certs:
+        if not ca_certs:
+            msg = (
+                "OPENSEARCH_VERIFY_CERTS is True but OPENSEARCH_CA_CERTS is not set."
+                "Not using SSL."
+            )
+            log.warning(msg)
+        else:
+            log.info("OPENSEARCH_VERIFY_CERTS is True. Verifying certificates.")
+            payload["ca_certs"] = ca_certs
+    return OpenSearch(**payload)
--- gateway/sds_gateway/api_methods/utils/opensearch_client.py
+++ gateway/sds_gateway/api_methods/utils/opensearch_client.py
@@ -1,30 +1,15 @@
 from django.conf import settings
-from loguru import logger as log
-from opensearchpy import OpenSearch
-from opensearchpy import RequestsHttpConnection
-from requests.auth import HTTPBasicAuth
+
+from common.sds_opensearch_query.client import build_opensearch_client
 
 
 def get_opensearch_client():
-    payload = {
-        "hosts": [{"host": settings.OPENSEARCH_HOST, "port": settings.OPENSEARCH_PORT}],
-        "http_auth": HTTPBasicAuth(
-            settings.OPENSEARCH_USER,
-            settings.OPENSEARCH_PASSWORD,
-        ),
-        "use_ssl": settings.OPENSEARCH_USE_SSL,
-        "verify_certs": settings.OPENSEARCH_VERIFY_CERTS,
-        "ssl_show_warn": False,
-        "connection_class": RequestsHttpConnection,
-    }
-    if settings.OPENSEARCH_VERIFY_CERTS:
-        if not settings.OPENSEARCH_CA_CERTS:
-            msg = (
-                "OPENSEARCH_VERIFY_CERTS is True but OPENSEARCH_CA_CERTS is not set."
-                "Not using SSL."
-            )
-            log.warning(msg)
-        else:
-            log.info("OPENSEARCH_VERIFY_CERTS is True. Verifying certificates.")
-            payload["ca_certs"] = settings.OPENSEARCH_CA_CERTS
-    return OpenSearch(**payload)
+    return build_opensearch_client(
+        host=settings.OPENSEARCH_HOST,
+        port=settings.OPENSEARCH_PORT,
+        user=settings.OPENSEARCH_USER,
+        password=settings.OPENSEARCH_PASSWORD,
+        use_ssl=settings.OPENSEARCH_USE_SSL,
+        verify_certs=settings.OPENSEARCH_VERIFY_CERTS,
+        ca_certs=settings.OPENSEARCH_CA_CERTS,
+    )
