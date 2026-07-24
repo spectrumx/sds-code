b79c17876e18d6eb8c9adfd7b65fa7b1355c1fcc
klpoland <kinsey.l.poland@gmail.com>
2026-07-24 13:00:04 -0400

    remove unused code

--- federation/sds_federation/services/peer_registry.py
+++ federation/sds_federation/services/peer_registry.py
@@ -16,7 +16,6 @@ class PeerRegistry:
 
     def register(self, hello: SiteHelloWebhook) -> None:
         self._peers[hello.site_name] = hello
-        self._last_seen[hello.site_name] = datetime.now(UTC)
 
     def get(self, site_name: str) -> SiteHelloWebhook | None:
         return self._peers.get(site_name)
