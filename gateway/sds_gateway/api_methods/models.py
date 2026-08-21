3bb0f38f97cc73109efb4bc897a8c7cbc84f7679
Matthew Curran <mcurran2@nd.edu>
2026-08-21 12:58:04 -0400

    fix:  Also forgot the is_deleted condition.

--- gateway/sds_gateway/api_methods/models.py
+++ gateway/sds_gateway/api_methods/models.py
@@ -1458,7 +1458,9 @@ class UserSharePermission(BaseModel):
         # if we get to here then check if the item is public
         if item_type in item_models:
             model_class = item_models[ItemType(item_type)]
-            if model_class.objects.filter(uuid=item_uuid, is_public=True).exists():
+            if model_class.objects.filter(
+                uuid=item_uuid, is_public=True, is_deleted=False
+            ).exists():
                 return PermissionLevel.VIEWER
 
         return None
